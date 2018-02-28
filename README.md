9793a029d1cf41cf5a2c6485c85a60625425f46f
Olekciy <0lekciy4@gmail.com>
2018-02-28 23:12:33 +0300

    Sloving 5_lang_frequency

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Frequency Analysis of Words
+The script compiles a list of frequently used words in the text.
+The path to the file with the text is passed as an argument when
+the file is started.
+# Example to run.
+```bash
+$ python3 lang_frequency.py text.txt
+1   ('и', 10137)
+2   ('в', 4863)
+3   ('не', 4221)
+4   ('что', 3652)
+5   ('на', 3200)
+6   ('с', 3025)
+7   ('он', 3017)
+8   ('как', 1913)
+9   ('его', 1876)
+10   ('к', 1778)
+```
 
-[TODO. There will be project description]
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,37 @@
+import re
+import sys
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r') as file_handler:
+            return file_handler.read()
+    except FileNotFoundError:
+        print('Bad path to file')
 
 
-def get_most_frequent_words(text):
-    pass
+def word_processing(text):
+    prepared_text = re.sub(r'\W', ' ', text)
+    words = prepared_text.split()
+    return words
+
+
+def creation_dictionary_words(words):
+    dictionary_words = Counter(words)
+    return dictionary_words
+
+
+def get_most_frequent_words(dictionary_words):
+    return dictionary_words.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+
+    text = load_data(sys.argv[1])
+    if text is not None:
+        words = word_processing(text)
+        dictionary_words = creation_dictionary_words(words)
+        most_frequent_words = get_most_frequent_words(dictionary_words)
+        for num, i in enumerate(most_frequent_words):
+            print(num + 1, " ", i)
