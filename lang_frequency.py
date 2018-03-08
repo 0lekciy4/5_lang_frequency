1986b1d5ae30bde672c5b2dc168d58c1f25ed501
Olekciy <0lekciy4@gmail.com>
2018-03-08 16:38:24 +0300

    revised remarks

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,38 +2,34 @@ import re
 import sys
 from collections import Counter
 
-TOP_WORDS = 10
-
 
 def load_data(filepath):
-    try:
         with open(filepath, 'r') as file_handler:
             return file_handler.read()
-    except FileNotFoundError:
-        print('Bad path to file')
 
 
-def word_processing(text):
-    prepared_text = re.sub(r'\W', ' ', text)
+def get_words(text):
+    prepared_text = re.sub(r'\W', ' ', text.lower())
     words = prepared_text.split()
     return words
 
 
-def creation_dictionary_words(words):
-    dictionary_words = Counter(words)
-    return dictionary_words
+def get_counter_words(words):
+    counter_words = Counter(words)
+    return counter_words
 
 
-def get_most_frequent_words(dictionary_words, top_words=TOP_WORDS):
-    return dictionary_words.most_common(top_words)
+def get_most_frequent_words(counter_words, top_words=10):
+    return counter_words.most_common(top_words)
 
 
 if __name__ == '__main__':
-
-    text = load_data(sys.argv[1])
-    if text is not None:
-        words = word_processing(text)
-        dictionary_words = creation_dictionary_words(words)
-        most_frequent_words = get_most_frequent_words(dictionary_words)
-        for num, word in enumerate(most_frequent_words):
-            print(num + 1, " ", word)
+    try:
+        text = load_data(sys.argv[1])
+        words = get_words(text)
+        counter_words = get_counter_words(words)
+        most_frequent_words = get_most_frequent_words(counter_words)
+        for num, count in enumerate(most_frequent_words, start=1):
+            print(num, " ", count)
+    except (FileNotFoundError, IndexError):
+        print('Bad path to file')
